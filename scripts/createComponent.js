92133304645831f3bdb738f743cef5f2c8586492
Jason Dalton <jcdalton@hotmail.com>
2019-01-01 10:57:30 -0600

    Create createComponent.js

---
+++ scripts/createComponent.js
@@ -0,0 +1,101 @@
+const chalk = require('chalk');
+const argv = require('minimist')(process.argv.slice(2));
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+class createComponent {
+  constructor() {
+    if(argv.name){
+      let paths = argv.name.split('/');
+      let name = argv.name;
+      let dir='';
+      if(paths.length > 1){
+        name = paths[paths.length - 1];
+        dir = `/${paths[0]}`;
+      }
+      console.log(chalk.green(`we are going to build component ${name}`));
+      this._buildDir(name,dir);
+      this._buildJS(name,dir);
+      this._buildHTML(name,dir);
+      this._buildSASS(name,dir);
+      this._buildTest(name,dir);
+    } else {
+      console.log(chalk.red('Please add the argument --name=<<name>>'));
+    }
+  }
+  _slug(name){
+    return name.toLowerCase().split('-').map((item) => {
+      return item.replace(/^\w/, c => c.toUpperCase())
+    }).join('');
+
+  }
+  _buildDir(name,dir){
+    mkdirp.sync(`src${dir}/tube-${name}`);
+    mkdirp.sync(`spec${dir}/tube-${name}`);
+    console.log(chalk.green(`we have created a file at src/tube-${name}`));
+  }
+  _buildJS(name,dir){
+    const file = `src${dir}/tube-${name}/tube-${name}.js`;
+    const writeStream = fs.createWriteStream(file);
+    writeStream.write(`
+export class Tube${this._slug(name)} extends HTMLElement {
+  constructor(){
+    super();
+
+  }
+  render(){
+    this.html(tempHtml);
+  }
+
+}
+if(!customElements.get('tube-${name}')){
+  customElements.define('tube-${name}', Tube${this._slug(name)});
+}
+`);
+    writeStream.end();
+    fs.createReadStream(file)
+      .pipe(writeStream);
+    console.log(chalk.green(`we have created javascript file tube-${name}.js`));
+  }
+  _buildHTML(name,dir){
+    const file = `src${dir}/tube-${name}/tube-${name}.html`;
+    const writeStream = fs.createWriteStream(file);
+    writeStream.write(`<div>${name}</div>`);
+    writeStream.end();
+    fs.createReadStream(file)
+      .pipe(writeStream);
+    console.log(chalk.green(`we have created html file devnav-${name}.html`));
+  }
+  _buildTest(name,dir){
+    const file = `spec${dir}/devnav-${name}/devnav-${name}-spec.js`;
+    const writeStream = fs.createWriteStream(file);
+    writeStream.write(`
+import '../../../dist${dir}/devnav-${name}';
+describe('Test of ${this._slug(name)}', ()=>{
+  let elem;
+  afterEach(()=>{
+    document.innerHTML = '';
+  });
+  beforeEach(()=>{
+    elem = document.createElement('devnav-${name}');
+  });
+  it('Test we can spec', ()=>{
+    document.body.appendChild(elem);
+    expect(!elem).toBeFalsy();
+  });
+
+});
+`);
+    writeStream.end();
+    fs.createReadStream(file)
+      .pipe(writeStream);
+    console.log(chalk.green(`we have created spec file devnav-${name}.js`));
+  }
+  _buildSASS(name,dir){
+    const file = `src${dir}/devnav-${name}/devnav-${name}.scss`;
+    const writeStream = fs.createWriteStream(file);
+    fs.createReadStream(file)
+      .pipe(writeStream);
+    console.log(chalk.green(`we have created sass file devnav-${name}.scss`));
+  }
+}
+new createComponent();
